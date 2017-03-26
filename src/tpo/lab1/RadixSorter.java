716217b5789b29ccba5b5956aeb1ccf28f0728a5
ynebytov <nebytov95@gmail.com>
2017-03-26 05:43:37 +0300

    changed RadixSorter, added tests for Radix

--- src/tpo/lab1/RadixSorter.java
+++ src/tpo/lab1/RadixSorter.java
@@ -6,15 +6,33 @@ package tpo.lab1;
 public class RadixSorter {
 
     private int[] digs = new int[10];
+    private StringBuilder returnValue = new StringBuilder();
 
-    public void sort(int[] array, int rank) {
+    private void addReturnValue(int[] array) {
+        returnValue.append("[");
+        returnValue.append(array[0]);
+        for (int i = 1; i < array.length; i++) {
+            returnValue.append("," + array[i]);
+        }
+        returnValue.append("]");
+    }
+
+    public String sort(int[] array) {
+        returnValue.delete(0, returnValue.length());
         int[] res = new int[array.length];
-        for(int i = 1; i <= rank; i++) {
+        for(int i = 1; true; i++) {
             Utils.copyArray(array, res);
+            addReturnValue(res);
             Utils.clearArray(digs);
             for (int k = 0; k < res.length; k++) {
                 digs[res[k]/((int)Math.pow(10,i-1))%10]++;
             }
+            if (digs[0] == array.length) {
+                break;
+            }
+            returnValue.append("\n");
+            addReturnValue(digs);
+            returnValue.append("\n");
             for (int k = 0; k < digs.length - 1; k++) {
                 digs[k+1] += digs[k];
             }
@@ -22,6 +40,7 @@ public class RadixSorter {
                 array[--digs[res[k]/((int)Math.pow(10,i-1))%10]] = res[k];
             }
         }
+        return returnValue.toString();
     }
 
 }
